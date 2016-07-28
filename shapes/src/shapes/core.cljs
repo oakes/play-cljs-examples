a0b7cd820168ff829d05d0e93f721da825c9ebdf
oakes <zsoakes@gmail.com>
2016-07-27 21:07:50 -0400

    Refactor

--- shapes/src/shapes/core.cljs
+++ shapes/src/shapes/core.cljs
@@ -1,15 +1,14 @@
 (ns shapes.core
-  (:require [play-cljs.core :as c]
-            [play-cljs.graphics :as g]))
+  (:require [play-cljs.core :as p]))
 
 (declare renderer)
 
 (def main-screen
-  (reify c/Screen
+  (reify p/Screen
     (on-show [_ state])
     (on-hide [_ state])
     (on-render [_ state timestamp]
-      (g/graphics
+      (p/graphics
         [:fill {:color 0xe74c3c :alpha 1}
          [:polygon {:path [0 0, 0 50, 50 50, 50 0]}
           [:fill {:color 0x9b59b6 :alpha 1}
@@ -22,14 +21,14 @@
         (-> state
             (assoc-in [:shape :x] (- (.-clientX event) (-> renderer .-view .-offsetLeft)))
             (assoc-in [:shape :y] (- (.-clientY event) (-> renderer .-view .-offsetTop)))
-            c/reset-state)))))
+            p/reset-state)))))
 
 (defonce renderer
-  (doto (c/create-renderer 500 500 {:background-color 0x65C25D})
+  (doto (p/create-renderer 500 500 {:background-color 0x65C25D})
     (->> .-view (.appendChild js/document.body))))
 
 (defonce game
-  (doto (c/create-game renderer {:shape {:x 0 :y 0}})
-    (c/start ["keydown" "mousemove"])
-    (c/set-screens [main-screen])))
+  (doto (p/create-game renderer {:shape {:x 0 :y 0}})
+    (p/start ["keydown" "mousemove"])
+    (p/set-screens [main-screen])))
 
