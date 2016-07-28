bef6b0fa0841b8d2cf328bc48ddbf682bd3a9951
oakes <zsoakes@gmail.com>
2016-07-27 20:10:23 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,10 @@
+## play-cljs-examples
+This repository provides public domain examples of games using [play-cljs](https://github.com/oakes/play-cljs).
+
+## Contents
+
+* **shapes** - a basic demo of drawing shapes
+
+## Licensing
+
+All files that originate from this project are dedicated to the public domain. I would love pull requests, and will assume that they are also dedicated to the public domain.
---
+++ shapes/src/shapes/core.cljs
@@ -0,0 +1,32 @@
+(ns shapes.core
+  (:require [play-cljs.core :as p]
+            [play-cljs.graphics :as g]))
+
+(defonce renderer
+  (doto (p/create-renderer 500 500 {:background-color 0x65C25D})
+    (->> .-view (.appendChild js/document.body))))
+
+(def main-screen
+  (reify p/Screen
+    (on-show [_ state])
+    (on-hide [_ state])
+    (on-render [_ state timestamp]
+      [(g/graphics
+         [:fill {:color 0xe74c3c :alpha 1}
+          [:polygon {:path [0 0, 0 50, 50 50, 50 0]}
+           [:fill {:color 0x9b59b6 :alpha 1}
+            [:rect {:x 10 :y 10 :width 20 :height 20}]
+            [:circle {:x 20 :y 40 :radius 10}]]]]
+         (get-in state [:shape :x])
+         (get-in state [:shape :y]))])
+    (on-event [_ state event]
+      (when (= (.-type event) "mousemove")
+        [(-> state
+             (assoc-in [:shape :x] (- (.-clientX event) (-> renderer .-view .-offsetLeft)))
+             (assoc-in [:shape :y] (- (.-clientY event) (-> renderer .-view .-offsetTop))))]))))
+
+(defonce game
+  (doto (p/create-game renderer {:shape {:x 0 :y 0}})
+    (p/start ["keydown" "mousemove"])
+    (p/set-screens [main-screen])))
+
