f3014bc25484d74b4e46599a721664a4aab05f05
oakes <zsoakes@gmail.com>
2016-07-31 16:45:59 -0400

    Add second screen

--- shapes/src/shapes/core.cljs
+++ shapes/src/shapes/core.cljs
@@ -9,21 +9,31 @@
   (reify p/Screen
     (on-show [_ state]
       (p/reset-state
-        {:background (p/graphics
-                       [:fill {:color 0x65C25D :alpha 1}
-                        [:rect {:x 0 :y 0 :width view-size :height view-size}]])
-         :shapes (p/graphics
-                   [:fill {:color 0xe74c3c :alpha 1}
-                    [:polygon {:path [0 0, 0 50, 50 50, 50 0]}
-                     [:fill {:color 0x9b59b6 :alpha 1}
-                      [:rect {:x 10 :y 10 :width 20 :height 20}]
-                      [:circle {:x 20 :y 40 :radius 10}]]]])
-         :label (p/text "Hello, world!" {:fill 0xFFFFFF :x 100 :y 100})}))
+        (assoc state
+          :background (p/graphics
+                        [:fill {:color 0x65C25D :alpha 1}
+                         [:rect {:x 0 :y 0 :width view-size :height view-size}]])
+          :label (p/text "Hello, world!" {:fill 0xFFFFFF :x 100 :y 100}))))
     (on-hide [_ state])
     (on-render [_ state]
       [(:background state)
-       (:label state)
-       (:shapes state)])
+       (:label state)])
+    (on-event [_ state event])))
+
+(def overlay-screen
+  (reify p/Screen
+    (on-show [_ state]
+      (p/reset-state
+        (assoc state
+          :shapes (p/graphics
+                    [:fill {:color 0xe74c3c :alpha 1}
+                     [:polygon {:path [0 0, 0 50, 50 50, 50 0]}
+                      [:fill {:color 0x9b59b6 :alpha 1}
+                       [:rect {:x 10 :y 10 :width 20 :height 20}]
+                       [:circle {:x 20 :y 40 :radius 10}]]]]))))
+    (on-hide [_ state])
+    (on-render [_ state]
+      (:shapes state))
     (on-event [_ state event]
       (when (= (.-type event) "mousemove")
         (let [x-offset (max 0 (- (.-clientWidth canvas) (.-clientHeight canvas)))
@@ -47,5 +57,5 @@
 (doto game
   (p/stop)
   (p/start ["mousemove"])
-  (p/set-screens [main-screen]))
+  (p/set-screens [main-screen overlay-screen]))
 
