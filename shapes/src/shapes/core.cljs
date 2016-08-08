f2654f73dd0940c22687f982ca045ba4a1678b47
oakes <zsoakes@gmail.com>
2016-08-08 00:56:24 -0400

    Update to 0.3.0

--- shapes/src/shapes/core.cljs
+++ shapes/src/shapes/core.cljs
@@ -3,30 +3,29 @@
 
 (declare game)
 
+(defonce state (atom {:shapes-x 0 :shapes-y 0}))
+
 (def main-screen
   (reify p/Screen
-    (on-show [_ state]
-      (p/set-state game
-        (assoc state
-          :text ["Hello, world!" {:x 0 :y 50 :size 16 :font "Georgia" :style :italic}
-                 ["Hi there" {:y 50 :size 32 :font "Helvetica" :style :normal}]])))
-    (on-hide [_ state])
-    (on-render [_ state]
-      (p/render game (:text state)))
-    (on-event [_ state event])))
+    (on-show [_])
+    (on-hide [_])
+    (on-render [_]
+      (p/render game ["Hello, world!" {:x 0 :y 50 :size 16 :font "Georgia" :style :italic}
+                      ["Hi there" {:y 50 :size 32 :font "Helvetica" :style :normal}]]))
+    (on-event [_ event])))
 
 (def overlay-screen
   (reify p/Screen
-    (on-show [_ state])
-    (on-hide [_ state])
-    (on-render [_ state]
-      (p/render game [:ellipse {:x (:shapes-x state) :y (:shapes-y state) :width 60 :height 60}
+    (on-show [_])
+    (on-hide [_])
+    (on-render [_]
+      (p/render game [:ellipse {:x (:shapes-x @state) :y (:shapes-y @state) :width 60 :height 60}
                       [:arc {:width 50 :height 50 :start 0 :stop 3.14}]
                       ;[:quad {:x1 -10 :y1 -15 :x2 10 :y2 -15 :x3 10 :y3 15 :x4 -10 :y4 15}]
                       [:rect {:x -10 :y -15 :width 20 :height 30}]
                       [:line {:x1 -10 :y1 -5 :x2 10 :y2 -5}]
                       [:triangle {:x1 -10 :y1 -15 :x2 10 :y2 -15 :x3 0 :y3 15}]]))
-    (on-event [_ state event]
+    (on-event [_ event]
       (when (= (.-type event) "mousemove")
         (let [canvas (p/get-canvas game)
               x-offset (max 0 (- (.-clientWidth canvas) (.-clientHeight canvas)))
@@ -35,8 +34,7 @@
               y-adjust (/ (p/get-height game) (- (.-clientHeight canvas) y-offset))
               new-x (* x-adjust (- (.-clientX event) (/ x-offset 2)))
               new-y (* y-adjust (- (.-clientY event) (/ y-offset 2)))]
-          (p/set-state game
-            (assoc state :shapes-x new-x :shapes-y new-y)))))))
+          (swap! state assoc :shapes-x new-x :shapes-y new-y))))))
 
 (defonce game (p/create-game 500 500))
 
--- super-koalio/src/super_koalio/core.cljs
+++ super-koalio/src/super_koalio/core.cljs
@@ -5,23 +5,26 @@
 
 (declare game)
 
+(defonce state (atom {}))
+
 (def main-screen
   (reify p/Screen
-    (on-show [_ state]
-      (p/set-state game (merge state (s/initial-state game))))
-    (on-hide [_ state])
-    (on-render [_ {:keys [x y direction] :as state}]
-      (let [x (if (= direction :left) (- x) x)]
+    (on-show [_]
+      (reset! state (s/initial-state game)))
+    (on-hide [_])
+    (on-render [_]
+      (let [{:keys [x y direction current]} @state
+            x (if (= direction :left) (- x) x)]
         (p/render game [:fill {:color "lightblue"}
                         [:rect {:width u/view-size :height u/view-size}
                          [:div {:x x :y y :width s/tile-width :height s/tile-height}
-                          (:current state)]]]))
-      (p/set-state game
-        (-> state
+                          current]]]))
+      (reset! state
+        (-> @state
             (s/move game)
             (s/prevent-move game)
             (s/animate))))
-    (on-event [_ state event])))
+    (on-event [_ event])))
 
 (defonce game (p/create-game u/view-size u/view-size))
 
